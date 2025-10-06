9c3f60bcaf49802d3fc7e24fdbdc17efef84e420
luchian <luchian-1999@mail.ru>
2025-10-06 23:10:56 +0300

    Full prototype is made. Overfitted model connected to backend and frontend.
    Note:
    1. Need to make inference in onnx format
    2. Collect data for full model and train it with metrics
    3. Connect database for data collection

---
+++ TextRecognition/backend/model_inf.py
@@ -0,0 +1,188 @@
+import io
+import base64
+import pickle
+
+from PIL import Image
+
+import fastapi
+from pydantic import BaseModel
+
+import torch
+import torchvision
+import torchsummary
+from torch.nn import utils
+from torch import nn, optim, onnx, distributions
+from torch.utils import data
+from torchvision import transforms
+from torchvision.transforms import functional
+
+import onnx
+import onnxruntime
+
+#global parameters
+D_MODEL = 512
+N_HEADS = 8
+NUM_LAYERS = 9
+
+
+
+#model data, processing tools, and functions
+
+#transform for PIL pictures
+def resnet_trans(pil_image):
+    """
+    Parameters
+    ----------
+    pil_image: image opened with pillow library 
+    
+    returns
+    ----------
+    Tensor image transformed for ResNet; with dtype float32
+    """
+    #apply necessary transformations for ResNet
+    data_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((224,224)),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    return data_transforms(pil_image)
+
+#transform for words
+def make_spaces(word):
+    """Makes spaces between the letter of a given word"""
+    return ' '.join(list(word))
+
+#function for positional embeddings
+def pos_embeds(embedding_matrix, dev = 'cpu'):
+    """
+    Parameters
+    ----------
+    embedding matrix: torch.Tensor of shape (batch_size, seq_len, embedding_dim)
+
+    returns
+    -------
+    embedding matrix + positional encoding
+    same embedding matrix with added positional embeddings
+    """
+    _, seq_len, embed_dim = tuple(embedding_matrix.shape)
+    #creating positional embedding matrix
+    pos_embeds = [] 
+    for pos in range(seq_len):
+        index_matrix = torch.arange(0, embed_dim).to(dtype = torch.float32, device=dev).reshape(1,-1)
+        #to even apply sin function
+        index_matrix[index_matrix%2 == 0] = torch.sin(pos/10_000**index_matrix[index_matrix%2 == 0]/embed_dim)
+        #to odd apply cos function but also with even indecies (that's why we extract 1)
+        index_matrix[index_matrix%2 == 1] = torch.cos(pos/10_000**((index_matrix[index_matrix%2 == 1]-1)/embed_dim))
+        pos_embeds.append(index_matrix)
+    #perform concatenation of resulting rows for each position
+    pos_embeds = torch.cat(pos_embeds)
+    #for each batch we add positional encodings
+    embedding_matrix = embedding_matrix + pos_embeds
+    return embedding_matrix
+
+#model inference
+def model_inference(pillow_picture, model, tokenizer, word_transform = make_spaces, picture_transform = resnet_trans, device = 'cpu', limit = 100):
+    """
+    Description
+    -----------
+    Given an image returns generated text
+    """
+    #change model state to evaluation
+    model.eval()
+    #transform picture for forward pass
+    tensor_picture = picture_transform(pillow_picture).unsqueeze(dim = 0).to(device = device)
+    #creating starting sequence
+    sequence = ''
+    #while last token does not equal to eos or limit achieved keep generating
+    iter_count = 0
+    while True and iter_count < limit:
+        #update count
+        iter_count += 1
+        sequence_tensor = torch.tensor(tokenizer(word_transform(sequence)).input_ids[:-1]).reshape(1,-1).to(device = device)
+        y_pred = model(x_pic_tensor = tensor_picture, x_letter_ids = sequence_tensor).squeeze(dim = 1)[-1].softmax(dim = 0) #choose the last token probabilities
+        #make distribution over vocabulary tokens
+        next_sampled_token = y_pred.argmax(dim = 0).detach().cpu().item()
+        #if we reached the last token we break (else continute)
+        if next_sampled_token == tokenizer._tokenizer.get_vocab()['<eos>']:
+            break
+        else:
+            #add new token
+            sequence += tokenizer._tokenizer.id_to_token(next_sampled_token)
+    return sequence
+
+
+#main model
+class TRModel(nn.Module):
+    def __init__(self, vocabulary_size, d_model = D_MODEL, nhead = N_HEADS, num_layers = NUM_LAYERS, padding_indx = 0, device = 'cpu'):
+        super().__init__()
+        self.device = device
+        #Encoder
+        #load ResNet with last two functions removed
+        self.encoder = nn.Sequential(*list(torchvision.models.resnet18(weights = torchvision.models.ResNet18_Weights.IMAGENET1K_V1, progress = False).children())[:-2])
+
+        #linear head that project to embedding dimension
+        self.linear_encoder_head = nn.Linear(512,d_model)
+
+        #Decoder
+        #embedding layer
+        self.embed_layer = nn.Embedding(num_embeddings=vocabulary_size, embedding_dim=d_model, padding_idx=padding_indx)
+        self.decoder_layer = nn.TransformerDecoderLayer(d_model=d_model, nhead=nhead)
+        self.decoder = nn.TransformerDecoder(self.decoder_layer, num_layers=num_layers)
+
+        #linear head over vocabulary
+        self.linear_head = nn.Linear(d_model,vocabulary_size)
+
+    def forward(self, x_pic_tensor, x_letter_ids):
+        """
+        parameters
+        ----------
+        x_pic: tensor of shape batch_size X channels X H x W
+        x_text: tensor of shape seq_len X batch_size X d_model (embed_dim)
+        """
+        encoder_forward = self.encoder(x_pic_tensor)
+        encoder_forward = encoder_forward.reshape(encoder_forward.shape[0], encoder_forward.shape[1], -1)
+        #reshape to shape (batch_size, spatial_sequence_len, num_channels)
+        encoder_forward = encoder_forward.transpose(1,2)
+        #adding positional embeddings
+        with torch.no_grad():
+            encoder_forward = pos_embeds(encoder_forward, dev = self.device)
+        #projecting number of channels to d_model shape
+        encoder_forward = self.linear_encoder_head(encoder_forward)
+        #reshaping to shape (seq_len, batch_size, embed_dim (d_model))
+        encoder_forward = encoder_forward.transpose(0,1)
+        
+        #getting vector embeddings
+        letter_embeds = self.embed_layer(x_letter_ids) 
+        #add positional embeddings and reshaping to seq_len first
+        with torch.no_grad():
+            letter_embeds = pos_embeds(letter_embeds, dev = self.device).transpose(0,1)
+        #going through decoder
+        decoder_forward = self.decoder(letter_embeds, memory = encoder_forward)
+        clf_head = self.linear_head(decoder_forward)
+        return clf_head
+    
+#laoding tokenizer
+with open('model_tokenizer.pkl','rb') as tokenizer_file:
+    model_tokenizer = pickle.load(tokenizer_file)
+
+#loading model weights
+model = TRModel(vocabulary_size=model_tokenizer._tokenizer.get_vocab_size())
+model.load_state_dict(torch.load(f='/home/luchian/all_data/ML_models_weights/TexRec_Overfit2_v1_weights.pth'))
+
+#request bodies
+class InputData(BaseModel):
+    """Request body for incoming data input"""
+    image_64_base: str
+
+
+#API
+app = fastapi.FastAPI()
+
+
+@app.post('/prediction')
+async def predict(data: InputData):
+    image = Image.open(io.BytesIO(base64.b64decode(data.image_64_base)))
+    prediction = model_inference(pillow_picture=image, model = model, tokenizer=model_tokenizer)
+    return {"generated_text": prediction}
+
+
